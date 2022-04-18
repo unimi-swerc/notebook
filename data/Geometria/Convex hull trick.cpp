8f1af328659d9bcc5f60a2d4b06ebd4457baa240
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-18 22:09:06 +0200

    Update Convex hull trick.cpp

--- data/Geometria/Convex hull trick.cpp
+++ data/Geometria/Convex hull trick.cpp
@@ -7,14 +7,15 @@ void add_line(ll k, ll b) {
         vecs.pop_back();
     }
     if(!hull.empty()) {
-        vecs.push_back(complex<ll>(1i) * (nw - hull.back()));
+        vecs.push_back(complex<ll>(1i)*(nw - hull.back()));
     }
     hull.push_back(nw);
 }
 
 ll get(ll x) {
     pt query = {x, 1};
-    auto it = lower_bound(vecs.begin(), vecs.end(), query, [](pt a, pt b) {
+    auto it = lower_bound(vecs.begin(), vecs.end(), query, 
+    [](pt a, pt b) {
         return cross(a, b) > 0;
     });
     return dot(query, hull[it - vecs.begin()]);
