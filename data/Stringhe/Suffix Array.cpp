1033e6c2338c8c2f263946a4cd52ba6a4af1c54b
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-04-18 18:40:15 +0200

    Suffix array

---
+++ data/Stringhe/Suffix Array.cpp
@@ -0,0 +1,19 @@
+vector<int> suffix_array(int N, string S) {
+  vector<int> ret(N), rnk(2 * N), tmp(N);
+  for (int i = 0; i < N; i++) {
+    ret[i] = i, rnk[i] = S[i];
+  }
+  for (int k = 1; k < N; k <<= 1) {
+    sort(ret.begin(), ret.end(), [&](int a, int b) {
+      return make_pair(rnk[a], rnk[a + k]) < make_pair(rnk[b], rnk[b + k]);
+    });
+    tmp[ret[0]] = 1;
+    for (int i = 1; i < N; i++) {
+      tmp[ret[i]] = tmp[ret[i - 1]] + (rnk[ret[i]] != rnk[ret[i - 1]]
+                            || rnk[ret[i] + k] != rnk[ret[i - 1] + k]);
+    }
+    copy(tmp.begin(), tmp.end(), rnk.begin());
+    if (rnk[ret.back()] == N) break;
+  }
+  return ret;
+}
