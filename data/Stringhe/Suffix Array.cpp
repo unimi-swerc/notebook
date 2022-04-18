96b07ab9433535a338d231b80a2494c2e2bab39d
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-18 22:37:37 +0200

    Update Suffix Array.cpp

--- data/Stringhe/Suffix Array.cpp
+++ data/Stringhe/Suffix Array.cpp
@@ -5,12 +5,14 @@ vector<int> suffix_array(int N, string S) {
   }
   for (int k = 1; k < N; k <<= 1) {
     sort(ret.begin(), ret.end(), [&](int a, int b) {
-      return make_pair(rnk[a], rnk[a + k]) < make_pair(rnk[b], rnk[b + k]);
+      return make_pair(rnk[a], rnk[a + k]) < 
+        make_pair(rnk[b], rnk[b + k]);
     });
     tmp[ret[0]] = 1;
     for (int i = 1; i < N; i++) {
-      tmp[ret[i]] = tmp[ret[i - 1]] + (rnk[ret[i]] != rnk[ret[i - 1]]
-                            || rnk[ret[i] + k] != rnk[ret[i - 1] + k]);
+      tmp[ret[i]] = tmp[ret[i - 1]] + 
+        (rnk[ret[i]] != rnk[ret[i - 1]]
+         || rnk[ret[i] + k] != rnk[ret[i - 1] + k]);
     }
     copy(tmp.begin(), tmp.end(), rnk.begin());
     if (rnk[ret.back()] == N) break;
