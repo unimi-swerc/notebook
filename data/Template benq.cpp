266d4507e9d5725ca4577fea1d8d78dc9fd60b3c
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-19 09:41:48 +0200

    Update Template benq.cpp

--- data/Template benq.cpp
+++ data/Template benq.cpp
@@ -1,3 +1,12 @@
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+//------------------------------------------------
+
 using ll = long long;
 using db = long double;
 using ul = uint64_t;
