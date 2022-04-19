64e5b873ec2fd93cff67fb87b4dc18e7b25db973
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-04-19 21:52:11 +0200

    fix tab

--- data/Geometria/Poligon Cut.cpp
+++ data/Geometria/Poligon Cut.cpp
@@ -7,18 +7,18 @@
 
 //pt e tutto il resto devono essere complex<double>
 vector<pt> polygonCut(const vector<pt>& poly, pt s, pt e) {
-	vector<pt> res;
-	rep(i,0,sz(poly)) {
-		pt cur = poly[i], prev = i ? poly[i-1] : poly.back();
-		bool side = cross(e-s, cur-s) < 0;
-		if (side != (cross(e-s, cur-s) < 0)){
-			pt out;
-			lineIntersection(line(s,e), line(cur,prev), out);
-			res.push_back(out);
-		}
-			
-		if (side)
-			res.push_back(cur);
-	}
-	return res;
+  vector<pt> res;
+  rep(i,0,sz(poly)) {
+    pt cur = poly[i], prev = i ? poly[i-1] : poly.back();
+    bool side = cross(e-s, cur-s) < 0;
+    if (side != (cross(e-s, cur-s) < 0)){
+      pt out;
+      lineIntersection(line(s,e), line(cur,prev), out);
+      res.push_back(out);
+    }
+      
+    if (side)
+      res.push_back(cur);
+  }
+  return res;
 }
