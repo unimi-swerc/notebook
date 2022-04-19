9c0c9a4670c52e5f1b4d5e1c561c097ea767398d
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-19 17:20:27 +0200

    Update XorSat.cpp

--- data/eXtra/XorSat.cpp
+++ data/eXtra/XorSat.cpp
@@ -16,10 +16,10 @@ using u32 = uint32_t;
   
   se non c'è soluzione r.size()=0
 */
-int gauss(vector<bitset<NSAT+1>> &a, int m, vector<bool> &ans) {
+int gauss(vector<bitset<NSAT+1>> &a,int m,vector<bool> &ans){
   int n = a.size();
     vector<int> where (m, -1);
-    for (int col = 0, row = 0; col < m && row < n; ++col) {
+    for (int col = 0, row = 0; col<m && row<n; ++col){
         for (int i = row; i < n; ++i)
             if (a[i][col]) {
                 swap (a[i], a[row]);
