5a82a79da962c1d44c9c7bb6997730e84768a7ef
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-04-19 15:23:39 +0200

    fix berlekamp massey

--- data/Matematica/Berlekamp Massey.cpp
+++ data/Matematica/Berlekamp Massey.cpp
@@ -1,12 +1,11 @@
-/*
- * Description: Recovers any $n$-order linear recurrence relation from the first
- * $2n$ terms of the recurrence.
- * Useful for guessing linear recurrences after brute-forcing the first terms.
- * Should work on any field, but numerical stability for floats is not guaranteed.
- * Output will have size $\le n$.
- * Usage: berlekampMassey({0, 1, 1, 3, 5, 11}) // {1, 2}
- * Time: O(N^2)
- */
+// Description: Recovers any n-order linear recurrence
+// relation from the first 2N terms of the recurrence.
+// Useful for guessing linear recurrences after brute-
+// forcing the first terms. Should work on any field,
+// but numerical stability for floats is not guaranteed.
+// Output will have size < n.
+// Usage: berlekampMassey({0, 1, 1, 3, 5, 11}) // {1, 2}
+// Time: O(N^2)
 
 const ll mod = 1000000007; // faster if const
 ll modpow(ll b, ll e) {
