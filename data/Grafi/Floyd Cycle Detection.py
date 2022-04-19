64b652ad7b1514fab9a127a2fba23dd5d8ebfb9a
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-19 14:06:41 +0200

    Add files via upload

---
+++ data/Grafi/Floyd Cycle Detection.py
@@ -0,0 +1,39 @@
+def floyd(f, x0):
+    # Main phase of algorithm: finding a repetition x_i = x_2i.
+    # The hare moves twice as quickly as the tortoise and
+    # the distance between them increases by 1 at each step.
+    # Eventually they will both be inside the cycle and then,
+    # at some point, the distance between them will be
+    # divisible by the period ?.
+    tortoise = f(x0) # f(x0) is the element/node next to x0.
+    hare = f(f(x0))
+    while tortoise != hare:
+        tortoise = f(tortoise)
+        hare = f(f(hare))
+  
+    # At this point the tortoise position, ?, which is also equal
+    # to the distance between hare and tortoise, is divisible by
+    # the period ?. So hare moving in circle one step at a time, 
+    # and tortoise (reset to x0) moving towards the circle, will 
+    # intersect at the beginning of the circle. Because the 
+    # distance between them is constant at 2?, a multiple of ?,
+    # they will agree as soon as the tortoise reaches index µ.
+
+    # Find the position µ of first repetition.    
+    mu = 0
+    tortoise = x0
+    while tortoise != hare:
+        tortoise = f(tortoise)
+        hare = f(hare)   # Hare and tortoise move at same speed
+        mu += 1
+ 
+    # Find the length of the shortest cycle starting from x_µ
+    # The hare moves one step at a time while tortoise is still.
+    # lam is incremented until ? is found.
+    lam = 1
+    hare = f(tortoise)
+    while tortoise != hare:
+        hare = f(hare)
+        lam += 1
+ 
+    return lam, mu
