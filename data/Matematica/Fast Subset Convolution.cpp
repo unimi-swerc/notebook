4824355cdc46cc7c5bb20bdf219dd2c70158b72b
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-19 10:50:54 +0200

    Add files via upload

---
+++ data/Matematica/Fast Subset Convolution.cpp
@@ -0,0 +1,20 @@
+// The size of a must be a power of two.
+// Time: O(N log N)
+typedef pair<__int128, __int128> pii;
+typedef vector<__int128> vi;
+void FST(vi& a, bool inv) {
+	for (int n = sz(a), step = 1; step < n; step *= 2) {
+		for (int i = 0; i < n; i += 2 * step) rep(j,i,i+step) {
+			__int128 &u = a[j], &v = a[j + step]; tie(u, v) =
+				inv ? pii(v - u, u) : pii(v, u + v); // AND 
+				// inv ? pii(v, u - v) : pii(u + v, u); // OR
+				// pii(u + v, u - v);                   // XOR
+		}
+	}
+	// if (inv) for (__int128& x : a) x /= sz(a); // XOR
+}
+vi conv(vi a, vi b) {
+	FST(a, 0); FST(b, 0);
+	rep(i,0,sz(a)) a[i] *= b[i];
+	FST(a, 1); return a;
+}
