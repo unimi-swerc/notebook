ab6214c08b32a844953628e4c30abfbc62c4789d
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-04-19 15:38:47 +0200

    fix hashing

--- data/Stringhe/Hashing.cpp
+++ data/Stringhe/Hashing.cpp
@@ -1,20 +1,21 @@
-// Arithmetic mod 2^64-1. 2x slower than mod 2^64 and more
-// code, but works on evil test data (e.g. Thue-Morse, where
-// ABBA... and BAAB... of length 2^10 hash the same mod 2^64).
-// "typedef ull H;" instead if you think test data is random,
-// or work mod 10^9+7 if the Birthday paradox is not a problem.
+// Arithmetic mod 2^64-1. 2x slower than mod 2^64 and
+// more code, but works on evil test data (e.g. Thue-Morse,
+// where ABBA... and BAAB... of length 2^10 hash the same
+// mod 2^64). "typedef ull H;" instead if you think test
+// data is random, or work mod 10^9+7 if the Birthday
+// paradox is not a problem.
 struct H {
   typedef uint64_t ull;
   ull x; H(ull x=0) : x(x) {}
 #define OP(O,A,B) H operator O(H o) { ull r = x; asm \
-  (A "addq %%rdx, %0\n adcq $0,%0" : "+a"(r) : B); return r; }
+  (A "addq %%rdx, %0\n adcq $0,%0" : "+a"(r) : B);return r;}
   OP(+,,"d"(o.x)) OP(*,"mul %1\n", "r"(o.x) : "rdx")
   H operator-(H o) { return *this + ~o.x; }
   ull get() const { return x + !~x; }
   bool operator==(H o) const { return get() == o.get(); }
   bool operator<(H o) const { return get() < o.get(); }
 };
-static const H C = (ll)1e11+3; // (order ~ 3e9; random also ok)
+static const H C = (ll)1e11+3;//(order~3e9; random also ok)
 
 struct HashInterval {
   vector<H> ha, pw;
@@ -36,9 +37,11 @@ vector<H> getHashes(string& str, int length) {
     h = h * C + str[i], pw = pw * C;
   vector<H> ret = {h};
   rep(i,length,sz(str)) {
-    ret.push_back(h = h * C + str[i] - pw * str[i-length]);
+    ret.push_back(h = h*C + str[i] - pw*str[i-length]);
   }
   return ret;
 }
 
-H hashString(string& s){H h{}; for(char c:s) h=h*C+c;return h;}
+H hashString(string& s) {
+  H h{}; for(char c:s) h=h*C+c;return h;
+}
