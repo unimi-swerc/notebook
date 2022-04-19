3dc968eb6b3c30f897a9fea14e002dd060953847
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-04-19 16:44:09 +0200

    fix simplesso

--- data/eXtra/Simplex.cpp
+++ data/eXtra/Simplex.cpp
@@ -1,17 +1,17 @@
-/**
- * Description: Solves a general linear maximization problem: 
- * maximize $c^T x$ subject to $Ax \le b$, $x \ge 0$.
- * Returns -inf if there is no solution, inf if there are arbitrarily good solutions, 
- * or the maximum value of $c^T x$ otherwise.
- * The input vector is set to an optimal $x$ (or in the unbounded case, 
- * an arbitrary solution fulfilling the constraints).
- * Usage:
- * vvd A = {{1,-1}, {-1,1}, {-1,-2}};
- * vd b = {1,1,-4}, c = {-1,-1}, x;
- * T val = LPSolver(A, b, c).solve(x);
- * Time: O(NM * \#pivots), where a pivot may be e.g. 
- * an edge relaxation. O(2^n) in the general case.
- */
+// Description: Solves a general linear maximization
+// problem: maximize c^T x subject to Ax<=b, x>=0.
+// Returns -inf if there is no solution, inf if there
+// are arbitrarily good solutions,  or the maximum value
+// of c^T x otherwise. The input vector is set to an
+// optimal x (or in the unbounded case, an arbitrary
+// solution fulfilling the constraints).
+// Usage:
+// vvd A = {{1,-1}, {-1,1}, {-1,-2}};
+// vd b = {1,1,-4}, c = {-1,-1}, x;
+// T val = LPSolver(A, b, c).solve(x);
+// Time: O(NM * \#pivots), where a pivot may be e.g. 
+// an edge relaxation. O(2^n) in the general case.
+
 #include<bits/stdc++.h>
 using namespace std;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
@@ -21,13 +21,14 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-typedef double T; // long double, Rational, double + mod<P>...
+typedef double T;
 typedef vector<T> vd;
 typedef vector<vd> vvd;
 
 const T eps = 1e-8, inf = 1/.0;
 #define MP make_pair
-#define ltj(X) if(s == -1 || MP(X[j],N[j]) < MP(X[s],N[s])) s=j
+#define ltj(X) if(s == -1 || \
+  MP(X[j],N[j]) < MP(X[s],N[s])) s=j
 
 struct LPSolver {
   int m, n;
@@ -37,7 +38,9 @@ struct LPSolver {
   LPSolver(const vvd& A, const vd& b, const vd& c) :
     m(sz(b)), n(sz(c)), N(n+1), B(m), D(m+2, vd(n+2)) {
       rep(i,0,m) rep(j,0,n) D[i][j] = A[i][j];
-      rep(i,0,m) { B[i] = n+i; D[i][n] = -1; D[i][n+1] = b[i];}
+      rep(i,0,m) {
+        B[i] = n+i; D[i][n] = -1; D[i][n+1] = b[i];
+      }
       rep(j,0,n) { N[j] = j; D[m][j] = -c[j]; }
       N[n] = -1; D[m+1][n] = 1;
     }
@@ -65,7 +68,7 @@ struct LPSolver {
       rep(i,0,m) {
         if (D[i][s] <= eps) continue;
         if (r == -1 || MP(D[i][n+1] / D[i][s], B[i])
-                     < MP(D[r][n+1] / D[r][s], B[r])) r = i;
+              < MP(D[r][n+1] / D[r][s], B[r])) r = i;
       }
       if (r == -1) return false;
       pivot(r, s);
@@ -77,7 +80,8 @@ struct LPSolver {
     rep(i,1,m) if (D[i][n+1] < D[r][n+1]) r = i;
     if (D[r][n+1] < -eps) {
       pivot(r, n);
-      if (!simplex(2) || D[m+1][n+1] < -eps) return -inf;
+      if (!simplex(2) || D[m+1][n+1] < -eps)
+        return -inf;
       rep(i,0,m) if (B[i] == -1) {
         int s = 0;
         rep(j,1,n+1) ltj(D[i]);
