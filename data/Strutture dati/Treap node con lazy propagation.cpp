0834b0aebdcfd79cbf5d0d866c0c6c36914c4dc5
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-04-20 13:27:47 +0200

    fix tab

--- data/Strutture dati/Treap node con lazy propagation.cpp
+++ data/Strutture dati/Treap node con lazy propagation.cpp
@@ -5,35 +5,35 @@ constexpr long long mod = 998244353;
 // N<=500K, Q<=500K (2900 ms)
 struct node {
     int val, mi;
-	T sum, lazy1, lazy2;
+  T sum, lazy1, lazy2;
     int size, rot;
     size_t prior;
     node *left, *right;
 
     node(T v) : val(v), mi(v), sum(v), lazy1(1), lazy2(0),
-	 size(1), rot(0), prior(rng()), left(0), right(0) {}
+   size(1), rot(0), prior(rng()), left(0), right(0) {}
 
-	static inline void combine(T a, T b, T &c, T &d){
-		c=(a*c)%mod;
-		d=(a*d+b)%mod;
-	}
+  static inline void combine(T a, T b, T &c, T &d){
+    c=(a*c)%mod;
+    d=(a*d+b)%mod;
+  }
 
     node* fix() {
         mi = val, size = 1, sum = val;
         
         if (left) {
-        	left->rot ^= rot;
+          left->rot ^= rot;
             mi = min(mi, left->mi);
             sum+=(left->sum*left->lazy1+
-			      left->size*left->lazy2);
+            left->size*left->lazy2);
             combine(lazy1,lazy2,left->lazy1,left->lazy2);
             size += left->size;
         }
         if (right) {
-        	right->rot ^= rot;
+          right->rot ^= rot;
             mi = min(mi, right->mi);
             sum+=(right->sum*right->lazy1+
-			      right->size*right->lazy2);
+            right->size*right->lazy2);
             combine(lazy1,lazy2,right->lazy1,right->lazy2);
             size += right->size;
         }
