c343fe0ccf57a494a01decc09bea55a8c73027b0
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-20 10:21:48 +0200

    Add files via upload

---
+++ data/eXtra/Divide and Conquer Dp.cpp
@@ -0,0 +1,33 @@
+/*
+This optimization for dynamic programming solutions uses the 
+concept of divide and conquer. It is only applicable
+ for the following recurrence:
+dp[i][j] = min_{k < j}(dp[i-1][k] + C[k][j])
+min[i][j] <= min[i][j+1]
+min[i][j] is the smallest k that gives the optimal answer
+This optimization reduces the time complexity 
+from O(KN^2) to O(KNlogN)
+*/
+int m, n;
+vector<long long> dp_before(n), dp_cur(n);
+
+long long C(int i, int j);
+// compute dp_cur[l], ... dp_cur[r] (inclusive)
+void compute(int l, int r, int optl, int optr) {
+    if (l > r)
+        return;
+
+    int mid = (l + r) >> 1;
+    pair<long long, int> best = {LLONG_MAX, -1};
+
+    for (int k = optl; k <= min(mid, optr); k++) {
+        best = min(best, {(k ? dp_before[k - 1] : 0) 
+		                            + C(k, mid), k});
+    }
+
+    dp_cur[mid] = best.first;
+    int opt = best.second;
+
+    compute(l, mid - 1, optl, opt);
+    compute(mid + 1, r, opt, optr);
+}
