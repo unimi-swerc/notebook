7fb2868236f8c59ba0fc8f546e454a77d3ae5352
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-04-20 14:35:09 +0200

    Update Template.cpp

--- data/eXtra/Template.cpp
+++ data/eXtra/Template.cpp
@@ -6,9 +6,7 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-
 //------------------------------------------------
-
 using ll = long long;
 using db = long double;
 using ul = uint64_t;
