7dc9bf4c01493308e15a803693789d700fa9bb30
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-06-06 11:58:34 +0200

    Update Max flow.cpp

--- data/Grafi/Max flow.cpp
+++ data/Grafi/Max flow.cpp
@@ -1,59 +1,56 @@
-// with n<=5000, m<=30000 run in 0.20 sec
-constexpr int MAXN = 6000;
-constexpr ll INF = 1e18;
-struct Edge {
-  int u, r;
-  ll f, c;
-};
-vector<Edge> grafo[MAXN];
-int level[MAXN];
-int cur[MAXN];
-bool levelGraph(int source, int sink) {
-  memset(level, 0, MAXN * sizeof(int));
-  memset(cur, 0, MAXN * sizeof(int));
-  level[source] = 1;
-  queue<int> Q;
-  Q.push(source);
-  while (!Q.empty()) {
-    int v = Q.front();
-    Q.pop();
-    for (Edge e : grafo[v]) {
-      if (level[e.u] == 0 && e.f < e.c) {
-        level[e.u] = level[v] + 1;
-        Q.push(e.u);
+// with n<=5000, m<=30000 run in 0.19 sec
+using pi = pair<int,int>;
+#define f first
+#define s second
+template<class T> using V = vector<T>; 
+using vi = V<int>;
+using vpi = V<pi>;
+#define sz(x) int((x).size())
+#define rsz resize
+#define pb push_back
+#define ft front()
+#define each(a,x) for (auto& a: x)
+ 
+template<class F> struct Dinic {
+  struct Edge { int to, rev; F cap; };
+  int N; V<V<Edge>> adj;
+  void init(int _N) { N = _N; adj.rsz(N); }
+  pi ae(int a, int b, F cap, F rcap = 0) { 
+    assert(min(cap,rcap) >= 0); // saved me > once
+    adj[a].pb({b,sz(adj[b]),cap});
+    adj[b].pb({a,sz(adj[a])-1,rcap});
+    return {a,sz(adj[a])-1};
+  }
+  F edgeFlow(pi loc) { // get flow along original edge
+    const Edge& e = adj.at(loc.f).at(loc.s);
+    return adj.at(e.to).at(e.rev).cap;
+  }
+  vi lev, ptr;
+  bool bfs(int s, int t) { // level=shortest dist from source
+    lev = ptr = vi(N);
+    lev[s] = 1; queue<int> q({s});
+    while (sz(q)) { int u = q.ft; q.pop();
+      each(e,adj[u]) if (e.cap && !lev[e.to]) {
+        q.push(e.to), lev[e.to] = lev[u]+1;
+        if (e.to == t) return 1;
       }
     }
+    return 0;
   }
-  return level[sink];
-}
-ll sendFlow(int v, ll flow, int sink) {
-  if (v == sink) return flow;
-  for (; cur[v] < grafo[v].size(); cur[v]++) {
-    Edge& e = grafo[v][cur[v]];
-    if (level[e.u] != level[v] + 1 || e.f == e.c) continue;
-    ll nxtFlow = sendFlow(e.u, min(flow, e.c - e.f), sink);
-    if (nxtFlow > 0) {
-      e.f += nxtFlow;
-      grafo[e.u][e.r].f -= nxtFlow;
-      return nxtFlow;
+  F dfs(int v, int t, F flo) {
+    if (v == t) return flo;
+    for (int& i = ptr[v]; i < sz(adj[v]); i++) {
+      Edge& e = adj[v][i];
+      if (lev[e.to]!=lev[v]+1||!e.cap) continue;
+      if (F df = dfs(e.to,t,min(flo,e.cap))) { 
+        e.cap -= df; adj[e.to][e.rev].cap += df;
+        return df; } // saturated >=1 one edge
     }
+    return 0;
   }
-  return 0;
-}
-void addEdge(int from, int to, ll cap) {
-  Edge a{to, (int)grafo[to].size(), 0, cap};
-  Edge b{from, (int)grafo[from].size(), 0, 0};
-  grafo[from].push_back(a);
-  grafo[to].push_back(b);
-}
-ll maxFlow(int source, int sink) {
-  ll flow = 0;
-  while (levelGraph(source, sink)) {
-    while (true) {
-      ll nxtFlow = sendFlow(source, INF, sink);
-      if (nxtFlow == 0) break;
-      flow += nxtFlow;
-    }
+  F maxFlow(int s, int t) {
+    F tot = 0; while (bfs(s,t)) while (F df = 
+      dfs(s,t,numeric_limits<F>::max())) tot += df;
+    return tot;
   }
-  return flow;
-}
+};
