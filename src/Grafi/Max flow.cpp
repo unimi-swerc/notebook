52ffe1709c54e9f441677e2a6a0b31cdc2c9fcd7
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-08-23 16:51:59 +0200

    Test rimossi

--- src/Grafi/Max flow.cpp
+++ src/Grafi/Max flow.cpp
@@ -1,4 +1,5 @@
 // 0-based, with n<=5000, m<=30000 run in 0.19 sec
+// per i grafi bipartiti N<=100000
 using pi = pair<int,int>;
 #define f first
 #define s second
--- src/Grafi/Max flow.test.cpp
+++
@@ -1,3 +0,0 @@
-// Testato fino a N<=5000
-// Per i grafi bipartiti N<=100000
-int main() {}
