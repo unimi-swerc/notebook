1f89f022e14dcd49fd633215c5e7d276566198a9
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2022-08-26 18:10:34 +0200

    Fix commenti

--- src/Matematica/Matrici/Matrix inverse mod.cpp
+++ src/Matematica/Matrici/Matrix inverse mod.cpp
@@ -1,8 +1,9 @@
 /* Invert matrix $A$ modulo a prime. Returns rank; result
  * is stored in $A$ unless singular (rank < n).
  * For prime powers, repeatedly set
- * $A^{-1} = A^{-1} (2I - AA^{-1}) (\text{mod }p^k)$
- * where $A^{-1}$ starts as the inverse of A mod p, and k is doubled in each step.
+ * $A^{-1} = A^{-1} (2I - AA^{-1}) \mod p^k$
+ * where $A^{-1}$ starts as the inverse of $A \mod p$,
+ * and $k$ is doubled in each step.
  * Time: $O(n^3)$
  */
 const ll mod = 998244353;//1000000007; // faster if const
