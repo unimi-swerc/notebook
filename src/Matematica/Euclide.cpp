5c5a8e35d30d18980586c1613444745bc47f5d7e
Alessandro Bortolin <43854807+bortoz@users.noreply.github.com>
2022-08-27 17:49:32 +0200

    Update Euclide.cpp

--- src/Matematica/Euclide.cpp
+++ src/Matematica/Euclide.cpp
@@ -1,16 +1,12 @@
-// Soluzione: (x + k*b/gcd(a,b), y - k*a/gcd(a,b))
-// restituisce (x, y, gcd(a,b))
+// Soluzione: $\left( x + k\cdot b / \gcd (a,b), y - k\cdot a / \gcd (a,b) \right)$
+// restituisce $\left( x, y, \gcd(a,b) \right)$
 tuple<ll, ll, ll> xgcd(ll a, ll b) {
-  ll s = 0, s2 = 1;
-  ll t = 1, t2 = 0;
+  ll s = 0, s2 = 1, t = 1, t2 = 0;
   while (a != 0) {
-    ll q = b / a;
-    b -= q * a;
-    s -= q * s2;
-    t -= q * t2;
+    ll q = b / a; b -= q * a;
+    s -= q * s2; t -= q * t2;
     swap(a, b);
-    swap(s, s2);
-    swap(t, t2);
+    swap(s, s2); swap(t, t2);
   }
   return {s, t, b};
 }
