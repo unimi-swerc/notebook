52b35189d4ee7b9cf301217d6f90a630ca047471
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-08-29 13:41:49 +0200

    Update Sat.cpp

--- src/altro/Sat.cpp
+++ src/altro/Sat.cpp
@@ -1,6 +1,13 @@
 using u32 = uint32_t;
 using u64 = uint32_t;
-//stesse convenzioni di xorsat
+/* 0-based, esempio d'uso:
+ * $(a \lor c \lor d) \wedge (b \lor \neg c \lor d) \wedge (a \lor b \lor \neg d)$
+ * $a = 0, \ \neg a = 1, \ b = 2, \ \neg b = 3, \ \ldots$
+ * vector<vector<u32>> v={{0, 4, 6},{2, 5, 6},{0, 2, 7}};
+ * auto r = sat(v);
+ * for (auto b : r) cout << b << " "; cout << endl;
+ * se non c'è soluzione r.size()=0
+ */
 
 vector<bool> sat(const vector<vector<u32>>& in) {
   u32 max_term = 0;
