33b8e40682218ed50eb3242579e621d821110546
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-09-08 12:06:13 +0200

    Update Gomory-Hu tree.cpp

--- src/Grafi/Gomory-Hu tree.cpp
+++ src/Grafi/Gomory-Hu tree.cpp
@@ -6,18 +6,18 @@
 * Time: $N-1$ calls to Dinic (use Max flow code)
  */
 template<class F> V<pair<pi,F>> gomoryHu(int N, 
-		const V<pair<pi,F>>& ed) { 
-	vi par(N); Dinic<F> D; D.init(N);
-	vpi ed_locs; each(t,ed)ed_locs.pb(D.ae(t.f.f,t.f.s,t.s,t.s));
-	V<pair<pi,F>> ans;
-	FOR(i,1,N) {
-		each(p,ed_locs) { // reset capacities
-			auto& e = D.adj.at(p.f).at(p.s);
-			auto& e_rev = D.adj.at(e.to).at(e.rev);
-			e.cap = e_rev.cap = (e.cap+e_rev.cap)/2;
-		}
-		ans.pb({{i,par[i]},D.maxFlow(i,par[i])});
-		FOR(j,i+1,N) if (par[j] == par[i] && D.lev[j]) par[j] = i;
-	}
-	return ans;
+    const V<pair<pi,F>>& ed) { 
+  vi par(N); Dinic<F> D; D.init(N);
+  vpi ed_locs; each(t,ed)ed_locs.pb(D.ae(t.f.f,t.f.s,t.s,t.s));
+  V<pair<pi,F>> ans;
+  FOR(i,1,N) {
+    each(p,ed_locs) { // reset capacities
+      auto& e = D.adj.at(p.f).at(p.s);
+      auto& e_rev = D.adj.at(e.to).at(e.rev);
+      e.cap = e_rev.cap = (e.cap+e_rev.cap)/2;
+    }
+    ans.pb({{i,par[i]},D.maxFlow(i,par[i])});
+    FOR(j,i+1,N) if (par[j] == par[i] && D.lev[j]) par[j] = i;
+  }
+  return ans;
 }
