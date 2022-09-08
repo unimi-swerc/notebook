676a19ecd923e75d26a15de80a2faa5803b2a34f
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-09-08 12:15:24 +0200

    Update Gomory-Hu tree.cpp

--- src/Grafi/Gomory-Hu tree.cpp
+++ src/Grafi/Gomory-Hu tree.cpp
@@ -1,14 +1,15 @@
-/** Description: Returns edges of Gomory-Hu tree (second element is weight). 
-* Max flow between pair of vertices of undirected graph is given by min edge
-* weight along tree path. Uses the fact that for any $i,j,k,$
-* $\lambda_{ik}\ge \min(\lambda_{ij},\lambda_{jk}),$ 
+/*Returns edges of Gomory-Hu tree (second element is weight) 
+* Max flow between pair of vertices of undirected graph 
+* is given by min edge weight along tree path. 
+* Uses the fact that for any $i,j,k,$ $\lambda_{ik}\ge \min(\lambda_{ij},\lambda_{jk}),$ 
 * where $\lambda_{ij}$ denotes the flow between $i$ and $j.$
 * Time: $N-1$ calls to Dinic (use Max flow code)
- */
+*/
 template<class F> V<pair<pi,F>> gomoryHu(int N, 
     const V<pair<pi,F>>& ed) { 
   vi par(N); Dinic<F> D; D.init(N);
-  vpi ed_locs; each(t,ed)ed_locs.pb(D.ae(t.f.f,t.f.s,t.s,t.s));
+  vpi ed_locs; 
+  each(t,ed)ed_locs.pb(D.ae(t.f.f,t.f.s,t.s,t.s));
   V<pair<pi,F>> ans;
   FOR(i,1,N) {
     each(p,ed_locs) { // reset capacities
@@ -17,7 +18,7 @@ template<class F> V<pair<pi,F>> gomoryHu(int N,
       e.cap = e_rev.cap = (e.cap+e_rev.cap)/2;
     }
     ans.pb({{i,par[i]},D.maxFlow(i,par[i])});
-    FOR(j,i+1,N) if (par[j] == par[i] && D.lev[j]) par[j] = i;
+    FOR(j,i+1,N)if(par[j]==par[i] && D.lev[j])par[j]=i;
   }
   return ans;
 }
