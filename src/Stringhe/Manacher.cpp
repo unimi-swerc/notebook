f244c0ba5047e952ec2b17ed1138dec08824f61a
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-10-11 12:08:07 +0200

    Update Manacher.cpp

--- src/Stringhe/Manacher.cpp
+++ src/Stringhe/Manacher.cpp
@@ -1,5 +1,8 @@
+/// Source: Bortoz
+/// Verification:
+/// https://www.spoj.com/problems/NUMOFPAL/ (id: 29983634)
 pair<vector<int>, vector<int>> manacher(string S) {
-  int N = S.size();
+  int N = S.size(); //Time: $\mathcal{O}(N)$
   vector<int> even(N), odd(N);
   for (int i=0, l=0, r=-1; i<N; i++) {
     int k = (i>r) ? 0 : min(even[l+r-i+1], r-i+1);
@@ -10,7 +13,7 @@ pair<vector<int>, vector<int>> manacher(string S) {
         l = i-k-1;
         r = i+k;
     }
-  }
+  }//even[i]=numpal centrati tra i-1 e i (even[0]=0)
   for (int i=0, l=0, r=-1; i<N; i++) {
     int k = (i>r) ? 1 : min(odd[l+r-i], r-i+1);
     while (0 <= i-k && i+k < N && S[i-k] == S[i+k])
@@ -20,6 +23,6 @@ pair<vector<int>, vector<int>> manacher(string S) {
       l = i-k;
       r = i+k;
     }
-  }
+  } //odd[i] = numpal centrati in i
   return {even, odd};
 }
