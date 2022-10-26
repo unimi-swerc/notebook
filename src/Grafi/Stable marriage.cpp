9a164fac5fc656960d2e271daae2645efd6add97
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-10-26 17:39:46 +0200

    Update Stable marriage.cpp

--- src/Grafi/Stable marriage.cpp
+++ src/Grafi/Stable marriage.cpp
@@ -1,13 +1,18 @@
+/// Source: Dariost (booklet SWERC 2019-2020 LaStatale Blue)
+/// https://github.com/dariost/swerc/blob/master/src/Extra/stablemp.cpp
+/// Verification:
+/// https://www.spoj.com/problems/STABLEMP/ (id: 29454835)
 // Gale-Shapley algorithm for the stable marriage problem.
 // madj[i][j] is the jth highest ranked woman for man i.
 // fpref[i][j] is the rank woman i assigns to man j.
+// rank = 0 -> first preference
 // Returns a pair of vectors (mpart, fpart), where mpart[i]
 // gives the partner of man i, and fpart is analogous.
-//tested with $n\le 500$ (0.04 sec)
+//tested with $N\leq 500$ (0.04 sec) ($\mathcal{O}(N^2)$)
 pair<vector<int>, vector<int>>
 stable_marriage(vector<vector<int>> &madj,
                 vector<vector<int>> &fpref) {
-  int n = madj.size();
+  int n = madj.size(); // 0-based
   vector<int> mpart(n, -1), fpart(n, -1);
   vector<int> midx(n);
   queue<int> mfree;
