698aa9cf2324f8ff907dd42a5340721921ca0e29
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-10-26 15:26:34 +0200

    Update Totient function.cpp

--- src/Matematica/Totient function.cpp
+++ src/Matematica/Totient function.cpp
@@ -1,3 +1,7 @@
+/// Source: Dariost (booklet SWERC 2019-2020 LaStatale Blue)
+/// https://github.com/dariost/swerc/blob/master/src/Extra/totient.cpp
+/// Verification:
+/// https://www.spoj.com/problems/ETF/ (id: 29454799) (fino a 10^6)
 // It took less than 0.5s to calculate with MAX=10^7
 #define MAX 10000000
 int phi[MAX];
