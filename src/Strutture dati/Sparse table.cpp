e0e9f4307b333b8df9caabd94b4b5b276661d970
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-10-27 09:53:04 +0200

    Update Sparse table.cpp

--- src/Strutture dati/Sparse table.cpp
+++ src/Strutture dati/Sparse table.cpp
@@ -1,6 +1,9 @@
+/// Source: Bortoz
+/// Verification:
+/// https://oj.uz/submission/648808
 constexpr int MAXN = 1 << 20, LOGN = 21;
 int table[LOGN][MAXN];
-void build(int N, int* V) {
+void build(int N, int* V) { //$\mathcal{O}(N\log{N})$
   copy(V, V + N, table[0]);
   for (int j = 1; j < LOGN; j++) {
     for (int i = 0; i + (1 << j) <= N; i++) {
@@ -9,7 +12,7 @@ void build(int N, int* V) {
     }
   }
 }
-int query(int l, int r) { // [l, r)
+int query(int l, int r) { //[l, r) ($\mathcal{O}(1)$)
   int k = 31 - __builtin_clz(r - l);
   return min(table[k][l], table[k][r-(1 << k)]);
 }
