ceea662907d20e1a630ac23017ebafa00c3036a6
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-10-28 19:46:44 +0200

    Update Gomory-Hu tree.cpp

--- src/Grafi/Gomory-Hu tree.cpp
+++ src/Grafi/Gomory-Hu tree.cpp
@@ -1,12 +1,16 @@
+/// Source:
+/// https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Flows%20(12.3)/GomoryHu.h
+/// Verification:
+/// https://www.spoj.com/problems/MCQUERY/ (id: 30319850)
 /*Returns edges of Gomory-Hu tree (second element is weight)
 * Max flow between pair of vertices of undirected graph 
 * is given by min edge weight along tree path. 
 * Uses the fact that for any $i,j,k,$ $\lambda_{ik}\ge \min(\lambda_{ij},\lambda_{jk}),$ 
 * where $\lambda_{ij}$ denotes the flow between $i$ and $j.$
 * Time: $N-1$ calls to Dinic (use Max flow code)
-*/
+* tested with $T\leq 10,N \leq 150,M \leq 3000,\text{edgeweight}\leq 10^6$(0.10 s)*/
 template<class F> V<pair<pi,F>> gomoryHu(int N, 
-    const V<pair<pi,F>>& ed) { 
+    const V<pair<pi,F>>& ed) { //nodes 0-based
   vi par(N); Dinic<F> D; D.init(N);
   vpi ed_locs; 
   each(t,ed)ed_locs.pb(D.ae(t.f.f,t.f.s,t.s,t.s));
@@ -21,4 +25,4 @@ template<class F> V<pair<pi,F>> gomoryHu(int N,
     FOR(j,i+1,N)if(par[j]==par[i] && D.lev[j])par[j]=i;
   }
   return ans;
-}
+} //works also for disconnected graphs
