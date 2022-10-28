a4a928fd4c3a68223850656d5106872a7392859f
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-10-28 16:34:56 +0200

    Update Convex hull trick.cpp

--- src/Geometria/Convex hull trick.cpp
+++ src/Geometria/Convex hull trick.cpp
@@ -1,6 +1,10 @@
+/// Source:
+/// https://cp-algorithms.com/geometry/convex_hull_trick.html
+/// Verification:
+/// https://www.hackerrank.com/contests/alten-coding-challenge/challenges/approvazione/submissions/code/1338547243
 vector<pt> hull, vecs;
 
-void add_line(ll k, ll b) {
+void add_line(ll k, ll b) { //ammortized $\mathcal{O}(n)$
   pt nw = {k, b};
   while(!vecs.empty()&&dot(vecs.back(),nw-hull.back())<0){
     hull.pop_back();
@@ -12,7 +16,7 @@ void add_line(ll k, ll b) {
   hull.push_back(nw);
 }
 
-ll get(ll x) {
+ll get(ll x) { //$\mathcal{O}(\log{n})$
   pt query = {x, 1};
   auto it = lower_bound(vecs.begin(), vecs.end(), query, 
               [](pt a, pt b) { return cross(a, b) > 0; });
