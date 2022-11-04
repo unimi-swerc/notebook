5f78c3511ff4ec09f1df6b0de709e9d58003341b
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-11-04 19:56:27 +0100

    Update Euclide.cpp

--- src/Matematica/Euclide.cpp
+++ src/Matematica/Euclide.cpp
@@ -2,6 +2,7 @@
 /// Verification:
 /// https://codeforces.com/contest/1728/submission/171410592
 /// https://open.kattis.com/submissions/8824728 (come subroutine di CRT)
+/// https://training.olinfo.it/#/task/ois_pouring/submissions (id: 887571)
 // Soluzione: $\left( x + k b / \gcd (a,b), \ y - k a / \gcd (a,b) \right)$
 // restituisce $\left( x, \ y, \ \gcd(a,b) \right)$
 tuple<ll, ll, ll> xgcd(ll a, ll b) {
@@ -13,4 +14,4 @@ tuple<ll, ll, ll> xgcd(ll a, ll b) {
     swap(s, s2); swap(t, t2);
   }
   return {s, t, b};
-}
+} //tested with ll=__int128, $a,b\leq 10^{18}$
