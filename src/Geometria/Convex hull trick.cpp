20b7f8eea3999448d1829c775411673ca73ab4c9
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2022-12-15 13:53:03 +0100

    Update Convex hull trick.cpp

--- src/Geometria/Convex hull trick.cpp
+++ src/Geometria/Convex hull trick.cpp
@@ -3,9 +3,9 @@
 /// Verification:
 /// https://www.hackerrank.com/contests/alten-coding-challenge/challenges/approvazione/submissions/code/1338547243
 vector<pt> hull, vecs;
-
+//add line k*x+b, k deve essere crescente
 void add_line(ll k, ll b) { //ammortized $\mathcal{O}(n)$
-  pt nw = {k, b};
+  pt nw = {k, b}; //rappresento la retta come un punto...
   while(!vecs.empty()&&dot(vecs.back(),nw-hull.back())<0){
     hull.pop_back();
     vecs.pop_back();
@@ -13,11 +13,11 @@ void add_line(ll k, ll b) { //ammortized $\mathcal{O}(n)$
   if(!hull.empty()) {
     vecs.push_back(complex<ll>(1i)*(nw - hull.back()));
   }
-  hull.push_back(nw);
+  hull.push_back(nw); // ...e tengo il lower convex hull
 }
-
+// min query
 ll get(ll x) { //$\mathcal{O}(\log{n})$
-  pt query = {x, 1};
+  pt query = {x, 1}; //cerca il minimo k*x+1*b
   auto it = lower_bound(vecs.begin(), vecs.end(), query, 
               [](pt a, pt b) { return cross(a, b) > 0; });
   return dot(query, hull[it - vecs.begin()]);
