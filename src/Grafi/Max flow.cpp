dc186fcd6fdc3ba78e4e78ab8c834cc372a384cd
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2023-01-10 19:19:10 +0100

    Update Max flow.cpp

--- src/Grafi/Max flow.cpp
+++ src/Grafi/Max flow.cpp
@@ -8,6 +8,7 @@
 /// https://codeforces.com/gym/102428/submission/186120441
 /// https://dmoj.ca/submission/5178029
 /// https://codeforces.com/contest/1070/submission/188615851
+/// https://codeforces.com/contest/1783/submission/188798255
 // 0-based, with $N\leq 5000, M\leq 30000$ run in 0.19 sec (F=ll)
 //$\mathcal{O}(N^2M)$, con grafi bipartiti $N\leq 10^5,M\leq 10^5$ (0.77 sec)
 using pi = pair<int,int>;
