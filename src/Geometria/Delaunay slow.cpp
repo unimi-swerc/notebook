d79b50a0b2f18c10c75aa13f64a27f796f175365
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2023-01-28 21:10:36 +0100

    Update Delaunay slow.cpp

--- src/Geometria/Delaunay slow.cpp
+++ src/Geometria/Delaunay slow.cpp
@@ -1,36 +1,37 @@
 /// Source: https://github.com/dariost/swerc/blob/master/src/Extra/delaunay.cpp
+/// Verification: https://acm.timus.ru/problem.aspx?space=1&num=1520
 // Slow but simple Delaunay triangulation. Does not handle
 // degenerate cases (from O'Rourke, Computational Geometry
 // in C), running time: $\mathcal{O}(n^4)$
 // INPUT: x[] = x-coordinates, y[] = y-coordinates
 // OUTPUT:   triples = a vector containing m triples of
 // indices corresponding to triangle vertices
-typedef double T;
+typedef double T; //tested with $N\leq 300,x_i^2+y_i^2\leq 1000$ (0.7 sec)
 typedef tuple<int, int, int> triple;
-vector<triple> delaunay(vector<T> &x, vector<T> &y) {
-  int n = x.size();
+vector<triple> delaunay(vector<T> &xx, vector<T> &yy) {
+  int n = xx.size();
   vector<T> z(n);
   vector<triple> ret;
   for (int i = 0; i < n; i++)
-    z[i] = x[i] * x[i] + y[i] * y[i];
+    z[i] = xx[i] * xx[i] + yy[i] * yy[i];
   for (int i = 0; i < n - 2; i++) {
     for (int j = i + 1; j < n; j++) {
       for (int k = i + 1; k < n; k++) {
         if (j == k) continue;
-        double xn = (y[j] - y[i]) * (z[k] - z[i]) -
-                    (y[k] - y[i]) * (z[j] - z[i]);
-        double yn = (x[k] - x[i]) * (z[j] - z[i]) -
-                    (x[j] - x[i]) * (z[k] - z[i]);
-        double zn = (x[j] - x[i]) * (y[k] - y[i]) -
-                    (x[k] - x[i]) * (y[j] - y[i]);
+        double xn = (yy[j] - yy[i]) * (z[k] - z[i]) -
+                    (yy[k] - yy[i]) * (z[j] - z[i]);
+        double yn = (xx[k] - xx[i]) * (z[j] - z[i]) -
+                    (xx[j] - xx[i]) * (z[k] - z[i]);
+        double zn = (xx[j] - xx[i]) * (yy[k] - yy[i]) -
+                    (xx[k] - xx[i]) * (yy[j] - yy[i]);
         bool flag = zn < 0;
         for (int m = 0; flag && m < n; m++) {
-          flag = flag && ((x[m] - x[i]) * xn + (y[m] -
-                 y[i]) * yn + (z[m] - z[i]) * zn <= 0);
+          flag = flag && ((xx[m] - xx[i]) * xn + (yy[m] -
+                yy[i]) * yn + (z[m] - z[i]) * zn <= 0);
         }
         if (flag) ret.emplace_back(i, j, k);
       }
     }
-  }
-  return ret;
-}
+  } //in case of degenerate cases (i.e. cyclic...
+  return ret; //... quadrilateral) it doesn't work... 
+} //... but it returns all possible triangles
