ed93ca8184a67055a60a6ce82c35d54d29a1c069
mrbrionix <fbrionix@gmail.com>
2023-02-13 18:11:47 +0100

    ultime rifiniture

--- src/altro/Divide and conquer DP.cpp
+++ src/altro/Divide and conquer DP.cpp
@@ -3,29 +3,29 @@
 /* This optimization for dynamic programming solutions uses
  * the concept of divide and conquer. It is only applicable
  * for the following recurrence:
- * dp[i][j] = min_{k < j}(dp[i-1][k] + C[k][j])
- * min[i][j] <= min[i][j+1]
- * min[i][j] is the smallest k that gives the optimal answer
+ * $\mathit{dp}[i][j] = \min_{k < j}(\mathit{dp}[i-1][k] + C[k][j])$
+ * $\min[i][j] \leq \min[i][j+1]$
+ * $\min[i][j]$ is the smallest $k$ that gives the optimal answer
  * This optimization reduces the time complexity 
- * from $\mathcal{O}(KN^2)$ to $\mathcal{O}(KN\log N)$*/
-int m, n;
-vector<long long> dp_before(n), dp_cur(n);
+ * from $\mathcal{O}(KN^2)$ to $\mathcal{O}(KN\log N)$ */
+int m, n; vector<long long> dp_before(n), dp_cur(n);
 long long C(int i, int j);
 // compute dp_cur[l], ... dp_cur[r] (inclusive)
 void compute(int l, int r, int optl, int optr) {
   if (l > r) return;
   int mid = (l + r) >> 1;
   pair<long long, int> best = {LLONG_MAX, -1};
-  for (int k = optl; k <= min(mid, optr); k++) {
+  for (int k = optl; k <= min(mid, optr); k++)
     best=min(best, {(k ? dp_before[k - 1]:0)+C(k,mid), k});
-  }
   dp_cur[mid] = best.first;
   int opt = best.second;
   compute(l, mid - 1, optl, opt);
   compute(mid + 1, r, opt, optr);
 }
 // versione $\mathcal{O}(n\log^2n)$ potenziata con ds ($\mathcal{O}(\log n)$ per upd)
-//d[l...r]= posizioni il cui il taglio ottimo è in [l_opt,r_opt]
+// $d[l\dots r]$ = posizioni il cui taglio ottimo è in $[l_{\mathit{opt}},r_{\mathit{opt}}]$
+// invariante di chiamata: 
+// sono sempre attivi gli upd in $[d[mid],\max(l_{\mathit{opt}}, d[mid]))$
 void divide_conquer(int l, int r, int l_opt, int r_opt){
   int mid = (l + r) / 2, opt = -1;
   ll best_delta = 1; //qua la ricorrenza è del tipo $min_{k > j}$
@@ -50,4 +50,4 @@ void divide_conquer(int l, int r, int l_opt, int r_opt){
     FOR(i, max(l_opt, d[mid]), r_opt + 1) upd(a[i], 1);
   }
   FOR(i, max(l_opt, d[mid]), r_opt + 1) upd(a[i], -1);
-}//invariante: sono sempre "attivi" gli upd in [d[mid],max(l_opt, d[mid]))
+}
--- src/altro/Knuth optimization DP.cpp
+++ src/altro/Knuth optimization DP.cpp
@@ -1,13 +1,13 @@
 /// Source:
 /// https://cp-algorithms.com/dynamic_programming/knuth-optimization.html
 /* Knuth's Optimization in dynamic programming 
-specifically applies for optimal tree problems. 
-It is only applicable for the following recurrence:
-dp[i][j] = min_{i < k < j}(dp[i][k] + dp[k][j] + C[i][j])
-min[i][j-1] <= min[i][j] <= min[i+1][j]
-min[i][j] is the smallest k that gives the optimal answer
-This optimization reduces the time complexity 
-from $\mathcal{O}(N^3)$ to $\mathcal{O}(N^2)$*/
+ * specifically applies for optimal tree problems. 
+ * It is only applicable for the following recurrence:
+ * $\mathit{dp}[i][j] = \min_{i < k < j}(\mathit{dp}[i][k] + \mathit{dp}[k][j] + C[i][j])$
+ * $\min[i][j-1] \leq \min[i][j] \leq \min[i+1][j]$
+ * $\min[i][j]$ is the smallest $k$ that gives the optimal answer
+ * This optimization reduces the time complexity 
+ * from $\mathcal{O}(N^3)$ to $\mathcal{O}(N^2)$ */
 long long dp[SIZE][SIZE],mid[SIZE][SIZE],pos[SIZE],n,m;
 cin>>p[1]>>p[2]>>...>>p[m];
 pos[0] = 0;pos[m + 1] = n;
