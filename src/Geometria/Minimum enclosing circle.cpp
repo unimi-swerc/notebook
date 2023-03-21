56d2bdbf221df2a73301eefd1520d658d043faf7
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-03-21 16:02:56 +0100

    Ottimizzato minimum enclosing circle

--- src/Geometria/Minimum enclosing circle.cpp
+++ src/Geometria/Minimum enclosing circle.cpp
@@ -1,59 +1,35 @@
 /// Source: https://github.com/dariost/swerc/blob/master/src/Extra/welzl.cpp
 /// Verification:
-/// https://www.spoj.com/problems/QCJ4/ (ID: 30771826)
-/// https://www.spoj.com/problems/ALIENS/ (ID: 30772237)
+/// https://www.spoj.com/problems/QCJ4/ (ID: 31063280)
 /* Welzl's algorithm: minimum enclosing circle in expected
  * linear time. If there are any duplicate points in the
  * input, be sure to remove them first. */
-struct point{ double x,y;};
-struct circle {
-  double x,y,r;
-  circle() {}
-  circle(double x, double y, double r)
-    : x(x), y(y), r(r) {}
-};
-circle b_md(vector<point> R) {
+typedef pair<pt, long double> circle;
+circle b_md(const vector<pt>& R) {
   if (R.size() == 0) {
-    return circle(0, 0, -1);
+    return {{0, 0}, -1};
   } else if (R.size() == 1) {
-    return circle(R[0].x, R[0].y, 0);
+    return {R[0], 0};
   } else if (R.size() == 2) {
-    return circle(
-        (R[0].x+R[1].x) / 2.0, (R[0].y+R[1].y) / 2.0,
-        hypot(R[0].x-R[1].x, R[0].y-R[1].y) / 2.0);
+    return {(R[0] + R[1]) * .5l, abs(R[0] - R[1]) * .5l};
   } else {
-    double D = (R[0].x-R[2].x) * (R[1].y-R[2].y) -
-               (R[1].x-R[2].x) * (R[0].y-R[2].y);
-    double p0 = (((R[0].x-R[2].x) * (R[0].x + R[2].x) +
-                  (R[0].y-R[2].y) * (R[0].y + R[2].y)) /
-                     2 * (R[1].y-R[2].y) -
-                 ((R[1].x-R[2].x) * (R[1].x + R[2].x) +
-                  (R[1].y-R[2].y) * (R[1].y + R[2].y)) /
-                     2 * (R[0].y-R[2].y)) / D;
-    double p1 = (((R[1].x-R[2].x) * (R[1].x + R[2].x) +
-                  (R[1].y-R[2].y) * (R[1].y + R[2].y)) /
-                     2 * (R[0].x-R[2].x) -
-                 ((R[0].x-R[2].x) * (R[0].x + R[2].x) +
-                  (R[0].y-R[2].y) * (R[0].y + R[2].y)) /
-                     2 * (R[1].x-R[2].x)) / D;
-    return circle(p0, p1, hypot(R[0].x-p0, R[0].y-p1));
+    pt center = circumCenter(R[0], R[1], R[2]);
+    return {center, abs(R[0] - center)};
   }
 }
-circle b_minidisk(vector<point> &P, int i,
-                  vector<point> R) {
+circle b_minidisk(vector<pt>& P, int i, vector<pt> R) {
   if (i == P.size() || R.size() == 3) {
     return b_md(R);
   } else {
-    circle D = b_minidisk(P, i + 1, R);
-    if (hypot(P[i].x - D.x, P[i].y - D.y) > D.r) {
+    auto [center, rad] = b_minidisk(P, i + 1, R);
+    if (abs(P[i] - center) > rad) {
       R.push_back(P[i]);
-      D = b_minidisk(P, i + 1, R);
+      return b_minidisk(P, i + 1, R);
     }
-    return D;
+    return {center, rad};
   }
 }
-// Call this function.
-circle minidisk(vector<point> P) {
-  random_shuffle(P.begin(), P.end());
-  return b_minidisk(P, 0, vector<point>());
-} //tested with $t\leq 5,n\leq 10^5$ (0.45 sec)
+circle minidisk(vector<pt> P) { // Call this function.
+  shuffle(P.begin(), P.end(), rng);
+  return b_minidisk(P, 0, {});
+}
