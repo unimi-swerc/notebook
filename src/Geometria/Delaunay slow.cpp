d5c9743b68a4d017de44cc9976a27a1ec41436ea
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-04-21 20:51:58 +0200

    Delaunay slow compresso

--- src/Geometria/Delaunay slow.cpp
+++ src/Geometria/Delaunay slow.cpp
@@ -6,30 +6,21 @@
  * INPUT: x[] = x-coordinates, y[] = y-coordinates
  * OUTPUT:   triples = a vector containing m triples of
  * indices corresponding to triangle vertices */
-typedef double T;//tested with $N\leq 300,x_i^2+y_i^2\leq 10^3$ (0.7 s)
-typedef tuple<int, int, int> triple;
-vector<triple> delaunay(vector<T> &xx, vector<T> &yy) {
-  int n = xx.size();
-  vector<T> z(n);
-  vector<triple> ret;
-  for (int i = 0; i < n; i++)
-    z[i] = xx[i] * xx[i] + yy[i] * yy[i];
-  for (int i = 0; i < n - 2; i++) {
-    for (int j = i + 1; j < n; j++) {
-      for (int k = i + 1; k < n; k++) {
+//tested with $N\leq 300,x_i^2+y_i^2\leq 10^3$ (0.7 s)
+auto delaunay(int N, vector<T> x, vector<T> y) {
+  vector<tuple<int, int, int>> ret;
+  vector<p3> p(N); // Richiede Base 3D
+  for (int i = 0; i < N; i++)
+      p[i] = {x[i], y[i], x[i]*x[i] + y[i]*y[i]};
+  for (int i = 0; i < N; i++) {
+    for (int j = i + 1; j < N; j++) {
+      for (int k = i + 1; k < N; k++) {
         if (j == k) continue;
-        double xn = (yy[j] - yy[i]) * (z[k] - z[i]) -
-                    (yy[k] - yy[i]) * (z[j] - z[i]);
-        double yn = (xx[k] - xx[i]) * (z[j] - z[i]) -
-                    (xx[j] - xx[i]) * (z[k] - z[i]);
-        double zn = (xx[j] - xx[i]) * (yy[k] - yy[i]) -
-                    (xx[k] - xx[i]) * (yy[j] - yy[i]);
-        bool flag = zn < 0;
-        for (int m = 0; flag && m < n; m++) {
-          flag = flag && ((xx[m] - xx[i]) * xn + (yy[m] -
-                yy[i]) * yn + (z[m] - z[i]) * zn <= 0);
-        }
-        if (flag) ret.emplace_back(i, j, k);
+        p3 t = (p[j] - p[i]) * (p[k] - p[i]);
+        bool ok = t.z < 0;
+        for (int m = 0; m < N; m++)
+            ok = ok && (((p[m] - p[i]) | t) <= 0);
+        if (ok) ret.emplace_back(i, j, k);
       }
     }
   } //in case of degenerate cases (i.e. cyclic...
