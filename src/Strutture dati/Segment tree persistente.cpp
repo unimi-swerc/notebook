abd3e11f033ab90e003ac7ab6fc4663b539faba5
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2023-04-24 10:38:36 +0200

    Update Segment tree persistente.cpp

--- src/Strutture dati/Segment tree persistente.cpp
+++ src/Strutture dati/Segment tree persistente.cpp
@@ -1,15 +1,14 @@
 /// Source: Dariost (booklet SWERC 2019-2020 LaStatale Blue)
 /// https://github.com/dariost/swerc/blob/master/src/Data_Structures/persistent_segment_tree.cpp
 /// Verification:
-/// https://evaluator.hsin.hr/tasks/HONI202167index/ (submission #1)
+/// https://evaluator.hsin.hr/tasks/HONI202167index/ (submission #3)
 // tested with $n\leq 2e5, q\leq 2e5\log{2e5}$ (1.145 sec)
 int segcnt = 0;
 struct segment {
   int l, r, lid, rid, sum;
 } segs[HUGE_VAL];
 int build(int l, int r) { //$\mathcal{O}(n)$
-  if(l > r)
-    return -1;
+  if(l > r) return -1;
   int id = segcnt++;
   segs[id].l = l;
   segs[id].r = r;
@@ -24,8 +23,7 @@ int build(int l, int r) { //$\mathcal{O}(n)$
   return id; //return the id of the new root
 }
 int update(int idx, int v, int id) { //$\mathcal{O}(\log{n})$
-  if(id == -1)
-    return -1;
+  if(id == -1) return -1;
   if(idx < segs[id].l || idx > segs[id].r)
     return id;
   int nid = segcnt++;
@@ -37,8 +35,7 @@ int update(int idx, int v, int id) { //$\mathcal{O}(\log{n})$
   return nid; //return the id of the new root
 }
 int query(int id, int l, int r) { //$\mathcal{O}(\log{n})$
-  if(r < segs[id].l || segs[id].r < l)
-    return 0;
+  if(r < segs[id].l || segs[id].r < l) return 0;
   if(l <= segs[id].l && segs[id].r <= r)
     return segs[id].sum;
   return query(segs[id].lid, l, r) +
