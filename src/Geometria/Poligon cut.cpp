1b79f3d4be0a888407847f2f5d645ca46208f41c
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2023-05-19 09:50:24 +0200

    Update Poligon cut.cpp

--- src/Geometria/Poligon cut.cpp
+++ src/Geometria/Poligon cut.cpp
@@ -1,13 +1,13 @@
 /// Source: https://github.com/kth-competitive-programming/kactl/blob/main/content/geometry/PolygonCut.h
-// Verification:
+/// Verification:
 /// https://icpc.kattis.com/submissions/11083198
 /* Returns a vector with the vertices of a polygon
  * with everything to the left of the line going
  * from s to e cut away. (Time: $\mathcal{O}(N)$). Usage:
  * vector<pt> p = ...; //va bene sia orario che antiorario
  * p = polygonCut(p, pt{0,0}, pt{1,0});
- *pt e tutto il resto devono essere complex<double>, tested
- *cutting a rectangle $10^4$ times (EPS = $10^{-3}$, 0.0 sec)*/
+ * pt e tutto il resto devono essere complex<double>, tested
+ * cutting a rectangle $10^4$ times (EPS = $10^{-3}$, 0.0 sec) */
 vector<pt> polygonCut(const vector<pt>& poly, pt s, pt e){
   vector<pt> res;
   rep(i,0,sz(poly)) {
