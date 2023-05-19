280077339683b350405498013adcebb2ac392d67
mrbrionix <fbrionix@gmail.com>
2023-05-19 17:39:15 +0200

    SMAWK testato

--- src/altro/SMAWK.cpp
+++ src/altro/SMAWK.cpp
@@ -2,28 +2,26 @@
 /// https://codeforces.com/blog/entry/110844
 /// https://noshi91.github.io/Library/algorithm/concave_max_plus_convolution.cpp
 /// Verification:
-/// https://oj.uz/submission/745219
-/// https://atcoder.jp/contests/abc218/submissions/41520329
+/// https://oj.uz/submission/745241
+/// https://atcoder.jp/contests/abc218/submissions/41520696
 /* smawk for (max,+) convolution, complexity: $\mathcal{O}(N)$
  * tested in a $\mathcal{O}(4N\log N)$ solution ($N\leq 2\cdot 10^5$, 913 ms)*/
 template <class Select>
 vector<int> smawk(const int row_size, const int col_size,
-  const Select &select){
-  using vec_zu = vector<int>;
-  const function<vec_zu(const vec_zu&, const vec_zu&)> solve=
-  [&](const vec_zu &row, const vec_zu &col) -> vec_zu{
+  const Select &sel){
+  using vi = vector<int>;
+  const function<vi(const vi&,const vi&)> solve =
+    [&](const vi &row, const vi &col) -> vi{
     const int n = row.size();
     if (n == 0) return {};
-    vec_zu c2;
+    vi c2, r2;
     for(const int i : col){
-      while(!c2.empty()&&select(row[c2.size()-1],c2.back(),i))
+      while(!c2.empty()&&sel(row[c2.size()-1],c2.back(),i))
         c2.pop_back();
       if (c2.size() < n) c2.push_back(i);
     }
-    vec_zu r2;
     for(int i = 1; i < n; i += 2) r2.push_back(row[i]);
-    const vec_zu a2 = solve(r2, c2);
-    vec_zu ans(n);
+    const vi a2 = solve(r2, c2); vi ans(n);
     for(int i = 0; i != a2.size(); i += 1) ans[i*2+1]=a2[i];
     int j = 0;
     for(int i = 0; i < n; i += 2){
@@ -31,28 +29,27 @@ vector<int> smawk(const int row_size, const int col_size,
       const int end = i + 1 == n ? c2.back() : ans[i + 1];
       while (c2[j] != end){
         j += 1;
-        if(select(row[i], ans[i], c2[j])) ans[i] = c2[j];
+        if(sel(row[i], ans[i], c2[j])) ans[i] = c2[j];
       }
     }
     return ans;
   };
-  vec_zu row(row_size);iota(row.begin(), row.end(), 0);
-  vec_zu col(col_size);iota(col.begin(), col.end(), 0);
+  vi row(row_size);iota(row.begin(), row.end(), 0);
+  vi col(col_size);iota(col.begin(), col.end(), 0);
   return solve(row, col);
 }
-template <class T>
-vector<T> concave_max_plus_convolution(const vector<T> &a,
-  const vector<T> &b){ // a qualsiasi, b convesso
+template <class T> // a qualsiasi, b convesso
+vector<T> conv(const vector<T> &a, const vector<T> &b){
   const int n = a.size(); const int m = b.size();
   const auto get = [&](const int i, const int j){
     return a[j] + b[i - j];
   };
-  const auto select=[&](const int i,const int j,const int k){
+  const auto sel=[&](const int i,const int j,const int k){
     if (i < k) return false;
     if (i - j >= m) return true;
     return get(i, j) <= get(i, k);
   };
-  const vector<int> amax = smawk(n + m - 1, n, select);
+  const vector<int> amax = smawk(n + m - 1, n, sel);
   vector<T> c(n + m - 1);
   for (int i = 0; i != n + m - 1; i += 1)
     c[i] = get(i, amax[i]);
