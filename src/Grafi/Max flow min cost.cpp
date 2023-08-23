5eacbd40c5edf01bbe073032948511036ffde4c5
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2023-08-23 19:45:59 +0200

    Update Max flow min cost.cpp

--- src/Grafi/Max flow min cost.cpp
+++ src/Grafi/Max flow min cost.cpp
@@ -5,6 +5,7 @@
 /// https://codeforces.com/contest/164/submission/197110813
 /// tal_fattorini
 /// https://training.olinfo.it/#/task/ois_rusco/statement (submission: 993510)
+/// https://qoj.ac/submission/148976
 /*Minimum-cost maximum flow, assumes no negative cycles.It's
  *possible to choose negative edge costs such that the first
  *run of Dijkstra (or SPFA) is slow, but this hasn't been an
