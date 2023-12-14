27ea5e62c04801ea98407ecf2f7752379879703b
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2023-12-14 15:25:38 +0100

    Update Fast subset convolution.cpp

--- src/Matematica/Fast subset convolution.cpp
+++ src/Matematica/Fast subset convolution.cpp
@@ -3,6 +3,7 @@
 /// Verification:
 /// https://judge.yosupo.jp/submission/86705 (and)
 /// https://judge.yosupo.jp/submission/86706 (xor)
+/// https://codeforces.com/contest/1906/submission/237087910 (xor)
 // The size of a,b MUST be a power of two.
 // Time: $\mathcal{O}(N \log N)$ (con $N\leq 2^{20}$ impiega 0.75 sec)
 typedef pair<__int128, __int128> pii; //pii e vi...
@@ -14,8 +15,8 @@ void FST(vi& a, bool inv) {
         inv ? pii(v - u, u) : pii(v, u + v); // AND
         // inv ? pii(v, u - v) : pii(u + v, u); // OR
         // pii(u + v, u - v);                   // XOR
-    }
-  }
+    } //OR: $A'[j] = \sum_i A[i] I_{i \subseteq j}$, AND: $A'[j] = \sum_i A[i] I_{i \supseteq j}$, XOR: $A'[j] = \sum_i A[i] (-1) ^ {|i \cap j|}$
+  } //GCD: $A'[j] = \sum_i A[i] I_{j \mid i}$, LCM: $A'[j] = \sum_i A[i] I_{i \mid j}$
   // if (inv) for (__int128& x : a) x /= sz(a); // XOR
 }
 vi conv(vi a, vi b) { //tested with $a_i,b_i<10^9,N\leq 2^{20}$
