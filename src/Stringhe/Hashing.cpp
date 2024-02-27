ae14d27b483937174848aa7d517020239d2c57ca
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2024-02-27 16:35:35 +0100

    Update Hashing.cpp

--- src/Stringhe/Hashing.cpp
+++ src/Stringhe/Hashing.cpp
@@ -9,6 +9,7 @@
 /// https://contest.ucup.ac/submission/220720
 /// https://www.acmicpc.net/source/70649010
 /// ois carry
+/// https://codeforces.com/contest/1923/submission/248548243
 // Polynomial hash for substrings with two bases
 mt19937 rng((uint64_t) chrono::duration_cast<chrono::
     nanoseconds>(chrono::high_resolution_clock::now()
