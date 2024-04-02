1d581e8a63762129d43dfe43142c4b4be1c40667
MrBrionix <34371486+MrBrionix@users.noreply.github.com>
2024-04-02 12:14:26 +0200

    Update Persistent lazy heap.cpp

--- src/Strutture dati/Persistent lazy heap.cpp
+++ src/Strutture dati/Persistent lazy heap.cpp
@@ -1,11 +1,16 @@
 /// Source: https://github.com/bqi343/cp-notebook/blob/master/Implementations/content/data-structures/LeftistHeap.h
 /// Verification:
-/// https://open.kattis.com/submissions/12063799 (usato lazy ma non persistent)
-/// https://judge.yosupo.jp/submission/167533 (usato persistente ma non lazy)
+/// https://open.kattis.com/submissions/13365429 (usato lazy ma non persistent)
+/// https://judge.yosupo.jp/submission/200139 (usato persistente ma non lazy)
+/// https://codeforces.com/contest/1942/submission/254690952 (usato persistente ma non lazy)
 /* Persistent meldable heap. Time: $\mathcal{O}(\log N)$ per meld.
  * Memory: $\mathcal{O}(\log N)$ per meld if copy==true, else $\mathcal{O}(1)$.*/
 typedef array<ll,2> KEY; //array<ll,3> per directed mst
-typedef struct Heap* ph; //con copy=true diventa persistente
+typedef shared_ptr<struct Heap> ph; //oppure struct Heap* ph;
+template<class... Args> //con copy=true diventa persistente
+static ph make_ph(Args&&... args){ //utile con la persistenza
+    return make_shared<Heap>(forward<Args>(args)...);
+} //altrimenti metti new Heap(forward<Args>(args)...);
 struct Heap { // min heap wrt KEY
   ph l = nullptr, r = nullptr;
   int s = 0; KEY v; // s: path to leaf
@@ -13,30 +18,30 @@ struct Heap { // min heap wrt KEY
   ll delta = 0;//x i lazy update, somma delta a tutto l'heap
   void prop(bool copy = false) {
     if(delta!=0){
-      v[0] += delta;
+      v[0] += delta; // chiave += delta
       if (l){
-        if(copy)l = new Heap(*l);
+        if(copy)l = make_ph(*l);
         l->delta += delta;
       }
       if (r){
-        if(copy)r = new Heap(*r);
+        if(copy)r = make_ph(*r);
         r->delta += delta;
       }
       delta = 0;
     }
   }
   KEY top() { prop(); return v; }
-};//$\mathcal{O}(n^2)$ meld,$n \leq 2500$->3.87 s|$\mathcal{O}(m)$ meld,$m \leq 3\cdot 10^5$->700 ms
-ph meld(ph p, ph q, bool copy = false) {
-  if (!p || !q) return p?:q;
+};//$\mathcal{O}(n^2)$ meld,$n \leq 2500$->4.0 s|$\mathcal{O}(m)$ meld,$m \leq 3\cdot 10^5$->750 ms
+ph meld(ph p, ph q, bool copy = false) { //i meld con $n \leq 2500$ sono...
+  if (!p || !q) return p?:q; //non persistenti e fatti con Heap*
   p->prop(copy), q->prop(copy);
   if (p->v > q->v) swap(p,q);
-  ph P = (copy ? new Heap(*p) : p);
+  ph P = (copy ? make_ph(*p) : p);
   P->r = meld(P->r,q);
   if (!P->l || P->l->s < P->r->s) swap(P->l,P->r);
   P->s = (P->r?P->r->s:0)+1; return P;
 } //esempio d'uso: vedi directed MST
 ph ins(ph p, KEY v,bool copy = false) {
-  if(p)p->prop(copy); return meld(p,new Heap(v),copy); }
+  if(p)p->prop(copy); return meld(p,make_ph(v),copy); }
 ph pop(ph p, bool copy = false) {
   p->prop(copy); return meld(p->l,p->r,copy); }
